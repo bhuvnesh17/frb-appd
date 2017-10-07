7785c2492d83d1fb7d12b06a3f4962e32c71fbca
Bhuvnesh Kumar <raghur171@gmail.com>
2017-10-06 19:23:19 -0700

    updated with working tests

--- src/test/java/com/appdynamics/extensions/sql/MetricCollectorTest.java
+++ src/test/java/com/appdynamics/extensions/sql/MetricCollectorTest.java
@@ -1,14 +1,21 @@
 package com.appdynamics.extensions.sql;
 
+import com.appdynamics.extensions.yml.YmlReader;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.junit.Test;
 
+import java.io.File;
 import java.math.BigDecimal;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,21 +29,44 @@ import org.mockito.Mockito;
  */
 public class MetricCollectorTest {
     private static final String METRIC_SEPARATOR = "|";
-    private String metricPrefix ;
-    private String dbServerDisplayName ;
+    private String metricPrefix = "metricPrefix";
+    private String dbServerDisplayName = "dbServer";
 
-    private String queryDisplayName ;
+    private String queryDisplayName = "queryName" ;
 
 
     @Test
-    public void testGoThroughResultSetCorrectValues(){
+    public void testGoThroughResultSetCorrectValues() throws SQLException {
         Map<String , BigDecimal> values = new HashMap<String, BigDecimal>();
 
         ResultSet resultSet = mock(ResultSet.class);
 
+        when(resultSet.next()).thenReturn(Boolean.TRUE,Boolean.FALSE);
 
 
+        int num1 = 6;
+        int num2 = 7;
+        when(resultSet.getString("TRN_TARGET_OPERATION")).thenReturn("metricPathName");
+        when(resultSet.getBigDecimal("TRN_FACADE_DURATION")).thenReturn(BigDecimal.valueOf(num1));
+        when(resultSet.getBigDecimal("TRN_ROUTER_DURATION")).thenReturn(BigDecimal.valueOf(num2));
 
+        Map queries = YmlReader.readFromFileAsMap(new File("src/test/resources/conf/config_for_columns.yml"));
+        ColumnGenerator columnGenerator = new ColumnGenerator();
+        List<Column> columns = columnGenerator.getColumns(queries);
+
+        MetricCollector metricCollector = new MetricCollector(metricPrefix,dbServerDisplayName,queryDisplayName);
+        Map<String , BigDecimal> resultFromAnswer = metricCollector.goThroughResultSet(resultSet,columns);
+
+        BigDecimal value1 = BigDecimal.valueOf(7);
+        BigDecimal value2 = BigDecimal.valueOf(6);
+
+        Assert.assertTrue(resultFromAnswer.containsKey("metricPrefix|dbServer|queryName|metricPathName|TRN_ROUTER_DURATION"));
+        Assert.assertTrue(value1 == resultFromAnswer.get("metricPrefix|dbServer|queryName|metricPathName|TRN_ROUTER_DURATION"));
+
+        Assert.assertTrue(resultFromAnswer.containsKey("metricPrefix|dbServer|queryName|metricPathName|TRN_FACADE_DURATION"));
+        Assert.assertTrue(value2 == resultFromAnswer.get("metricPrefix|dbServer|queryName|metricPathName|TRN_FACADE_DURATION"));
+
+        Assert.assertTrue(resultFromAnswer.size() == 2);
     }
 
 
--- src/test/java/com/appdynamics/extensions/sql/MetricPrinterTest.java
+++ src/test/java/com/appdynamics/extensions/sql/MetricPrinterTest.java
@@ -4,11 +4,14 @@ import com.appdynamics.extensions.util.MetricWriteHelper;
 import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 /**
@@ -21,6 +24,17 @@ public class MetricPrinterTest {
     private String metricPrefix = "Custom Metrics|Tibco ASG|";
     private String displayName = "FRB-Try";
 
+
+
+    @Test
+    public void reportNodeMetricsTest () {
+        MetricPrinter metricPrinter = new MetricPrinter( metricWriter);
+
+        String metricName = "MetricName";
+        BigDecimal value = BigDecimal.valueOf(89);
+        metricPrinter.reportMetric(metricName, value);
+//        verify(metricPrinter, times(1)).reportMetric(metricName, value);
+    }
 
 
 }
